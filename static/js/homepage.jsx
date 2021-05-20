dfb82d99404deabd175100bb175c151f96cd8660
Khanh Mai <khanhmai@creighton.edu>
2021-05-20 16:11:34 +0000

    initial commit
    co-authored by: Kavya Nagarajan <kavya.0219@gmail.com>

---
+++ static/js/homepage.jsx
@@ -0,0 +1,11 @@
+"use strict";
+
+function Homepage() {
+  return (
+    <div>
+      This component needs to be implemented.
+    </div>
+  );
+}
+
+ReactDOM.render(<Homepage />, document.querySelector('#app'));
